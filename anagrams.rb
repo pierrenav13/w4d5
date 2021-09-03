a79e3b7beb3d60f6c3e3b63fe69cc79ee9ebc53d
Emmay <emmayalam1@gmail.com>
2021-09-03 14:29:26 -0400

    refactored and added anagrams methods in different time complexities

--- anagrams.rb
+++ anagrams.rb
@@ -10,10 +10,9 @@ end
 def second_anagram?(str_1, str_2)
     str_array = str_2.chars
     str_1.each_char do |char|
-        if str_2.include?(char)
-            idx = str_array.index(char)
-            str_array.delete_at(idx)
-        end
+      idx = str_array.find_index(char)
+      return false if idx.nil?
+      str_array.delete_at(idx)
     end
     str_array.empty?
 end
@@ -26,11 +25,33 @@ def third_anagram?(str_1, str_2)
    arr_1 = str_1.split('').sort
    arr_2 = str_2.split('').sort
    arr_1 == arr_2
-end 
-
-p third_anagram?("gizmo", "sally")
-p third_anagram?("elvis", "lives")
-
-def fourth_anagram?
-
 end
+
+# p third_anagram?("gizmo", "sally")
+# p third_anagram?("elvis", "lives")
+
+def fourth_anagram?(str_1, str_2)
+  hash1 = Hash.new(0)
+  hash2 = Hash.new(0)
+
+  str_1.each_char { |char| hash1[char] += 1 }
+  str_2.each_char { |char| hash2[char] += 1 }
+
+  hash1 == hash2
+end
+
+# p fourth_anagram?("gizmo", "sally")
+# p fourth_anagram?("elvis", "lives")
+
+def bonus_anagram?(str_1, str_2)
+  hash = Hash.new(0)
+
+  str_1.each_char { |char| hash[char] += 1 }
+  str_2.each_char { |char| hash[char] -= 1 }
+
+  hash.each_value { |sum| return false unless sum == 0 }
+  true
+end
+
+# p bonus_anagram?("gizmo", "sally")
+# p bonus_anagram?("elvis", "lives")
